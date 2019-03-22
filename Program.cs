416b2536d1fe3f476f43d0c95f47b2178a6b19ea
Kenely Cristina de Oliveira <kenely.oliveira@ltm.digital>
2019-03-22 11:36:25 -0300

    Commit

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.IO;
+
+namespace NETCoreWebJobs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new HostBuilder();
+            builder.ConfigureWebJobs(b =>
+            {
+                b.AddAzureStorageCoreServices();
+                b.AddAzureStorage();
+            });
+            builder.ConfigureLogging((context, b) =>
+            {
+                b.AddConsole();
+            });
+            var host = builder.Build();
+            using (host)
+            {
+                host.Run();
+            }
+        }
+
+        //private static void SetupConfiguration(IConfigurationBuilder builder)
+        //{
+        //    Configuration = new ConfigurationBuilder()
+        //        .SetBasePath(Directory.GetCurrentDirectory())
+        //        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+        //        .AddEnvironmentVariables()
+        //        .Build();
+        //}
+
+        //private static void ConfigureServices(IServiceCollection services)
+        //{
+        //    //services.AddDbContext<RaizenDBContext>(options =>
+        //    //{
+        //    //    options.UseSqlServer(Configuration.GetConnectionString("RaizenResgateFacilDB"));
+        //    //});
+
+        //    //services.Configure<JwtAuthenticationOptions>(Configuration.GetSection("Authentication:Jwt"));
+
+        //    //services.AddSingleton(Configuration);
+        //    //services.AddTransient<IEventNotificationRepository, EventNotificationRepository>();
+        //    //services.AddTransient<IEventNotificationService, EventNotificationService>();
+        //}
+    }
+}
---
+++ TimerTrigger.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace NETCoreWebJobs
+{
+    public class TimerTrigger
+    {
+        public static void ProcessQueueMessage([QueueTrigger("kenely")] string message, ILogger logger)
+        {
+            Console.WriteLine($"message {message}");
+
+            logger.LogInformation(message);
+        }
+    }
+}
